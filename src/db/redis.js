a996dc401434ff6977c8ea023959267f42cf4954
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-10 23:56:47 +0530

    reconfigured redis

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
 import { Redis } from "ioredis";
 
-export const redisClient = new Redis(process.env.REDIS_URL)
+export const redisClient = new Redis({
+    host: process.env.REDIS_SERVICE_NAME,
+    port:process.env.REDIS_PORT
+})
