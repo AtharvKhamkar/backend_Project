1c6192922fe93e378d41c8f596b18cb040fb04c8
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-11 00:06:28 +0530

    new redis port changed

--- src/db/redis.js
+++ src/db/redis.js
@@ -3,5 +3,5 @@ import { Redis } from "ioredis";
 
 export const redisClient = new Redis({
     host: process.env.REDIS_SERVICE_NAME,
-    port:process.env.REDIS_PORT
+    port:process.env.REDIS_PORT || 6381
 })
