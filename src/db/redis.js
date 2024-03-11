7360997bea76f10e3fd379099ce8f85a48099ea5
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-11 09:17:06 +0530

    reset redis

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,10 +1,6 @@
-import dotenv from "dotenv";
 import { Redis } from "ioredis";
-dotenv.config({
-    path: './env'
-})
 
 export const redisClient = new Redis({
-    host: process.env.REDIS_SERVICE_NAME,
-    port:process.env.REDIS_PORT || 6381
+    host: "localhost",
+    port:6379
 })
