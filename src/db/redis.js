1281dfa37b267f92aa406fe33098f950fc3e5b35
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-11 16:21:58 +0530

    add tls redis

--- src/db/redis.js
+++ src/db/redis.js
@@ -4,7 +4,8 @@ export const redisClient = new Redis({
     host:process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     username:process.env.REDIS_USER,
-    password:process.env.REDIS_PASSWORD,
+    password: process.env.REDIS_PASSWORD,
+    tls:process.env.REDIS_HOST
 })
 
 redisClient.connect(()=>console.log("redis connected"))
