9389fb4a66ef3d66c193a5904538c10ed0dfc352
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-16 15:34:24 +0530

    add swagger

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
+import path from "path";
+import postmanToOpenApi from "postman-to-openapi";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import { app } from "./app.js";
 import connectDB from './db/index.js';
 
@@ -8,6 +12,16 @@ import connectDB from './db/index.js';
 
 dotenv.config({
     path: './env'
+})
+
+postmanToOpenApi(
+    "postman/Backend_Project(Server).postman_collection.json",
+    path.join("postman/swagger.yml"),
+    {defaultTag:"General"}
+).then((response) => {
+    let result = YAML.load("postman/swagger.yml");
+    result.servers[0].url = "/";
+    app.use("/swagger",swaggerUi.serve,swaggerUi.setup(result))
 })
 
 
