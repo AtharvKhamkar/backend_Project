b4df534ff04d7a2d35189b8c6de3ab0e405a6ba5
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-06-14 17:46:24 +0530

    add winston and morgan logger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import morgan from 'morgan';
+import logger from './utils/logger.js';
 
 
 const app = express();
@@ -21,6 +23,22 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+const morganFormat = ':method :url :status :response-time ms';
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+  
+        };
+        logger.info(JSON.stringify(logObject));
+      }
+    }
+  }));
 
 //router import 
 import commentRouter from "./routes/comment.routes.js";
